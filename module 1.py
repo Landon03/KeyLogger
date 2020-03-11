0492c46d160d77f078ae9634df5bfa1cd4b43f64
Landon03 <55545837+Landon03@users.noreply.github.com>
2020-03-11 17:36:51 -0400

    Update module 1.py

--- module 1.py
+++ module 1.py
@@ -1,8 +1,16 @@
+"""
+
+https://github.com/Landon03/Python_Scripts
+
+"""
+
+
+import os
 FirstName1 = input("What is your first name: ").upper()
 LastName2 = input("What is your last name: ").upper()
 class Person:
     def __init__(self):
-        print("____Your-Name___")
+        print("Name Printer by Landon Jacobs")
 
     def FullName(self, firstname, lastname):
         self.firstname = firstname
@@ -13,3 +21,5 @@ class Person:
 personname = Person()
 personname.FullName(FirstName1, LastName2)
 personname.PrintFullname()
+os.system("pause")
+os.system("exit")
