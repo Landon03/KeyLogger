6ec1583abd9fa1d7d0e2505925a2c13d5ee3b531
Landon03 <55545837+Landon03@users.noreply.github.com>
2020-03-14 02:30:58 -0400

    Add files via upload

---
+++ H or T.py
@@ -0,0 +1,19 @@
+"""
+Title: Heads or Tails in python
+made by Landon Jacobs
+https://github.com/Landon03/Python_Scripts
+all rights reserved 2020
+"""
+import random
+import os
+a = (random.randint(1,2))
+# Tails 
+if a == 1:
+	print("Tails")
+	os.system("pause")
+	os.system("exit")
+# Heads
+if a == 2:
+	print("heads")
+	os.system("pause")
+	os.system("exit")
